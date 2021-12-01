146e210637467073d1e022c44c8c2f993c8c4320
motyo066 <mogyorosi.ferenc@nav.gov.hu>
2021-12-01 14:14:50 +0100

    második lecke

---
+++ ts-basic-practice002-array-tuple-enum/ts-basic-practice002-array-tuple-enum/src/lesson-01.ts
@@ -0,0 +1,41 @@
+export default class calculator {
+    // A változókat az osztályon belül így hozhatod létre.
+    active: boolean = false;
+
+    // Hozd létre a következő változókat a megfelelő típus annotációval.
+    
+    /** @type {Array} points - számok tömbje, 1 - 10 -ig egyesével. */
+    points: Array<number> = [1,2,3,4,5,6,7,8,9,10]; 
+
+    /** 
+     * A tömb minden eleme legyen egy objektum, ami egy felhasználót reprezentál.
+     * Kötelező elemek: name<string>, email<string>.
+     * Inicializáld a tömböt, legyen legalább két eleme.
+     * @type {Array} users - minden elem egy felhasználó.
+     */
+     users: Array<{name: string, email: string}> = 
+     [
+         { name: "Mogyorósi Ferenc", email: "mogyorosi.ferenc@nav.gov.hu"},
+         { name: "Mogyorósini Futó Kinga", email: "mogyorosine.kinga@nav.gov.hu"}
+     ];
+    
+    /** 
+     * A tömb minden eleme legyen egy objektum, ami egy terméket reprezentál.
+     * Kötelező elemek: name<string>, price<number>, stock<number>.
+     * Opcionális elemek: active<boolean>.
+     * Inicializáld a tömböt, legyen legalább két eleme.
+     * @type {Array} products - minden elem egy termék
+     */    
+     products: Array<{name: string, price: number, stock: number, active?: boolean}> = 
+     [
+         { name: "Mogyorósi Ferenc", price: 10, stock: 1},
+         { name: "Mogyorósini Futó Kinga", price: 20, stock: 3, active: true}
+     ];
+    
+    /** 
+     * Inicializálj egy tuple típust, azaz adj neki értéket is.
+     * @type {Tuple} role - number, string, boolean. 
+     */
+     role: [number, string, boolean];
+
+}
